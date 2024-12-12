c906583f527afc96e3b7c091c661ec829cb3ab0b
clearlynew <140883761+clearlynew@users.noreply.github.com>
2024-12-12 12:17:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+function diceRoll(){
+    let roll=document.getElementById("dice");
+    let randnum=Math.floor(Math.random()*6)+1;
+    let randimg="./images/dice"+randnum+".png";
+    roll.setAttribute("src",randimg);
+    addScore(randnum);
+}
+var greensq=1;
+function addScore(randnum){
+    document.querySelector("h1").innerHTML="Come Play! ";
+    document.querySelector(".block"+greensq).classList.remove("green");
+    greensq+=randnum;
+    if (greensq > 64) {
+        greensq = 64;
+    }
+    if( document.querySelector(".block"+greensq).classList.contains("red")){
+        greensq=1;
+        document.querySelector("h1").innerHTML="Try Again ";
+    }
+
+    document.querySelector(".block"+greensq).classList.add("green");
+
+    if (greensq===64){
+        document.querySelector("h1").innerHTML="You Won!";
+    }
+}
